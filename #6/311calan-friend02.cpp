bde32da9ac133b701f5fee3d7743777549da4b69
wishshukorn <102361217+wishshukorn@users.noreply.github.com>
2022-08-02 11:04:56 +0700

    Add files via upload

---
+++ #6/311calan-friend02.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+int i, d, m, b=1, c, a=1;
+printf("day");
+scanf("%d",&d);
+printf("month");
+scanf("%d",&m);
+for(i=1;i<=5;i++){
+ for(c=1;c<=7;c++){
+    for(a;a<=d-1;a++){
+        printf("_\t");
+        c=c+1;
+    }
+     if(b<=m){
+     printf("%d\t",b);
+     b=b+1;    
+     }
+ }
+ printf("\n");
+}
+}
---
+++ #6/311calan-friend03.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+int i,d,m,a;
+printf("day");
+scanf("%d",&d);
+printf("month");
+scanf("%d",&m);
+a=d-1;
+for(i=1;i<=m;i++){
+  a++;
+  for(d;d-1>0;d--){
+         printf("_\t");
+     }
+  printf("%d\t",i);
+  if(a%7==0){
+          printf("\n");
+       }
+}
+}
